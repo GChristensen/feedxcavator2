1935f6d74d72a9d472430473a41231ed6c8f1057
GChristensen <gchristnsn@gmail.com>
2019-03-13 12:29:50 +0400

    fixed update script

--- update.cmd
+++ update.cmd
@@ -1,13 +1,13 @@
 @set PATH=%PATH%;%INSTALLS%/appengine-sdk/bin
 @rem appcfg --oauth2 update ./war & appcfg --oauth2 backends ./war update worker
 
-appcfg --oauth2 update ./war
+appcfg --oauth2 update ./war & @ren .\war\WEB-INF\appengine-web.xml appengine-web.1 & @ren .\war\WEB-INF\appengine-web.2 appengine-web.xml & appcfg --oauth2 update ./war & @ren .\war\WEB-INF\appengine-web.xml appengine-web.2 & @ren .\war\WEB-INF\appengine-web.1 appengine-web.xml
 
-@ren .\war\WEB-INF\appengine-web.xml appengine-web.1
-@ren .\war\WEB-INF\appengine-web.2 appengine-web.xml
+@rem @ren .\war\WEB-INF\appengine-web.xml appengine-web.1
+@rem @ren .\war\WEB-INF\appengine-web.2 appengine-web.xml
 
-appcfg --oauth2 update ./war
+@rem appcfg --oauth2 update ./war
 
-@ren .\war\WEB-INF\appengine-web.xml appengine-web.2
-@ren .\war\WEB-INF\appengine-web.1 appengine-web.xml
+@rem @ren .\war\WEB-INF\appengine-web.xml appengine-web.2
+@rem @ren .\war\WEB-INF\appengine-web.1 appengine-web.xml
 
